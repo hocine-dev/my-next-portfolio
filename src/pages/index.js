32eb83eeb58d3fa8910a0d53c2a386bf09aff099
Hocine HAMAMA <47196223+hocine-dev@users.noreply.github.com>
2025-01-15 11:10:53 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,7 @@
 import Layout from "@/components/Layout";
 import Head from "next/head";
-import Image from "next/image";
-import mainImg from "../../public/images/home/mainimg.jpg";
-import circularImg from "../../public/images/home/circular.png";
-import LightBulb from "../../public/images/svgs/miscellaneous_icons_1.svg";
 import AnimatedText from "@/components/AnimatedText";
 import Link from "next/link";
-import HomeStyle from "../styles/Home.module.css";
 import TransitionEffect from "@/components/TransitionEffect";
 import BackToTop from "@/components/BackToTop";
 import { useState, useEffect } from "react";
@@ -16,12 +11,10 @@ export default function Home() {
 
   useEffect(() => {
     if (typeof window !== "undefined") {
-      // Check if userAgentData is available (for browsers like Chrome)
       if (window.navigator.userAgentData && window.navigator.userAgentData.mobile) {
         setIsMobile(window.navigator.userAgentData.mobile);
       } else {
-        // Fallback to checking screen width for mobile detection
-        setIsMobile(window.innerWidth <= 768); // You can adjust the width as needed
+        setIsMobile(window.innerWidth <= 768);
       }
     }
   }, []);
@@ -37,12 +30,13 @@ export default function Home() {
         <meta name="google-site-verification" content="zwvBd7kHHWhKEObp6vwWEf3LhnqbmAYhPJq1U51B7Jo" />
       </Head>
 
-      <TransitionEffect></TransitionEffect>
+      <TransitionEffect />
 
       <main className="flex items-center justify-center text-dark w-full min-h-screen xs:min-h-[90vh] dark:text-light lg:mt-3">
         <Layout className="text-center">
           <AnimatedText
-            text="We&apos;ll Be Back Soon!"
+            // eslint-disable-next-line react/no-unescaped-entities
+            text="We'll Be Back Soon!"
             className="xs:!text-3xl !text-center xl:!text-5xl lg:!text-6xl md:!text-5xl sm:!text-3xl lg:p-1"
           />
 
